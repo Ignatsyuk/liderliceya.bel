c763b49037fd20e458968e7bc64166acb34aa7ad
Alexander Ignatyuk <54143543+Merikosu@users.noreply.github.com>
2023-09-19 20:34:27 +0300

    Deployment

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="page">
-    hellow
+    Женёк – лучший!
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/pages/HomeView.vue'
+import HomeView from '@/pages/PageHome.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/pages/AboutView.vue')
-    }
   ]
 })
 
